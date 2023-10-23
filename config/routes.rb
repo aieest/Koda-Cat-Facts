1c5cae21d56c911d91f70e20ba2d96e114e773c0
August Estidola <aie.estidola@gmail.com>
2023-10-23 13:23:07 +0800

    generated post controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :cat_facts, only: [:index, :create, :destroy]
   root 'cat_facts#index'
+
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_025657) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_051839) do
   create_table "cat_facts", charset: "utf8mb4", force: :cascade do |t|
     t.text "fact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_cat_facts_on_user_id"
+  end
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
